e7006289ea013f3bad37d2f41b6f4ca9cf648dc6
Songsky267 <71706116+Songsky267@users.noreply.github.com>
2022-09-13 16:51:08 +0900

    수정
    응 수정

---
+++ Zay.Shop/templatemo_559_zay_shop/assets/js/templatemo.js
@@ -0,0 +1,75 @@
+/*
+
+TemplateMo 559 Zay Shop
+
+https://templatemo.com/tm-559-zay-shop
+
+*/
+
+'use strict';
+$(document).ready(function() {
+
+    // Accordion
+    var all_panels = $('.templatemo-accordion > li > ul').hide();
+
+    $('.templatemo-accordion > li > a').click(function() {
+        console.log('Hello world!');
+        var target =  $(this).next();
+        if(!target.hasClass('active')){
+            all_panels.removeClass('active').slideUp();
+            target.addClass('active').slideDown();
+        }
+      return false;
+    });
+    // End accordion
+
+    // Product detail
+    $('.product-links-wap a').click(function(){
+      var this_src = $(this).children('img').attr('src');
+      $('#product-detail').attr('src',this_src);
+      return false;
+    });
+    $('#btn-minus').click(function(){
+      var val = $("#var-value").html();
+      val = (val=='1')?val:val-1;
+      $("#var-value").html(val);
+      $("#product-quanity").val(val);
+      return false;
+    });
+    $('#btn-plus').click(function(){
+      var val = $("#var-value").html();
+      val++;
+      $("#var-value").html(val);
+      $("#product-quanity").val(val);
+      return false;
+    });
+    $('.btn-size').click(function(){
+      var this_val = $(this).html();
+      $("#product-size").val(this_val);
+      $(".btn-size").removeClass('btn-secondary');
+      $(".btn-size").addClass('btn-success');
+      $(this).removeClass('btn-success');
+      $(this).addClass('btn-secondary');
+      return false;
+    });
+    // End roduct detail
+
+    var audio = new Audio('KakaoTalk_Audio_20220913_1642_44_274');
+audio.volume = 0.1;
+audio.autoplay = true;
+
+$('.trigger').click(function() {
+  if (audio.paused == false) {
+      audio.pause();
+      $('.fa-play').show();
+      $('.fa-pause').hide();
+      $('.music-card').removeClass('playing');
+  } else {
+      audio.play();
+      $('.fa-pause').show();
+      $('.fa-play').hide();
+      $('.music-card').addClass('playing');
+  }
+});
+
+});
